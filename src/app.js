49b86f2369346bfccb8c291ae522072345a2b6df
Sy Lian <shengyou.lian@thscom.co.jp>
2023-04-02 19:18:45 +0900

    modify intent & port

--- src/app.js
+++ src/app.js
@@ -1,26 +1,26 @@
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require('express')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
 const middleware = require('./common/middleware')
 
-var inquiryRouter = require('./routes/inquiry.route');
+var inquiryRouter = require('./routes/inquiry.route')
 
-var app = express();
+var app = express()
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 app.use(middleware.response)
 
 // 一覧取得API
-app.use('/', inquiryRouter);
+app.use('/', inquiryRouter)
 
 app.get('/test', (req, res) => {
-    res.send('test')
+	res.send('test')
 })
 
-const port = 4001
+const port = 4000
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
-module.exports = app;
+module.exports = app
--- src/common/middleware.js
+++ src/common/middleware.js
@@ -1,9 +1,9 @@
 /* aaaa */
 exports.response = (req, res, next) => {
-    if (res.statusCode === 200) {
-        res.setHeader('test', 'test')
-    } else {
-        res.setHeader('err', 'err')
-    }
-    next()
+	if (res.statusCode === 200) {
+		res.setHeader('test', 'test')
+	} else {
+		res.setHeader('err', 'err')
+	}
+	next()
 }
